bd1e663e2d4879b98b57aef916042c21e0650a25
migmosquera <migmosquera@hotmail.com>
2017-01-17 17:08:42 -0400

    “subiendo_envio_correo_”

--- controller/sendEmail.php
+++ controller/sendEmail.php
@@ -1,6 +1,7 @@
 <?php
 	$msjSendEmail = "";
     if(isset($_POST['sendEmail'])){
+    	require_once 'controller/email/class.smtp.php';
     	require_once 'controller/email/PHPMailerAutoload.php';
     	require_once 'controller/email/class.phpmailer.php';
     	$name = $_POST['name'];
@@ -10,12 +11,12 @@
 		$correo = new PHPMailer();
 		$correo->IsSMTP();
 		$correo->SMTPAuth = true;
-		$correo->SMTPSecure = 'ssl';
-		$correo->Host = "smtp.zoho.com";
-		$correo->Port = 465;
-		$correo->Username = "administrador@poporoink.com.ve";
-		$correo->Password = "syslife12*";
-		$correo->setFrom("administrador@poporoink.com.ve");
+		$correo->SMTPSecure = 'tls';
+		$correo->Host = "smtp.gmail.com";
+		$correo->Port = 587;
+		$correo->Username = "soportesyslife@gmail.com";
+		$correo->Password = "equipo_syslife";
+		$correo->setFrom("soportesyslife@gmail.com");
 		$correo->addAddress('migmosquera2303@gmail.com');
 		$correo->Subject = "correo enviado por '.$name.'";
 		$correo->MsgHTML("<strong>Mensaje enviado por el usuario:</strong> $name </br> <strong>Email:</strong> $email  </br> <strong>Mensaje:</strong> $msjEmail  ");
